bd3577990ff2806384df57bb57f383c71c4775f2
herve <simon@dufflecoat.io>
2025-10-09 14:43:34 +1000

    instance type info

--- src/GTMH.S11n/FieldTypes/InstanceField.cs
+++ src/GTMH.S11n/FieldTypes/InstanceField.cs
@@ -92,7 +92,7 @@ namespace GTMH.S11n.FieldTypes
 
     public void WriteVisitation(Code code)
     {
-      code.WriteLine($"a_Visitor.VisitInstance(\"{this.Name}\", {Attrs.Required.ToString().ToLower()});");
+      code.WriteLine($"a_Visitor.VisitInstance(\"{this.Name}\", \"{InterfaceType.Replace("?", "")}\", {Attrs.Required.ToString().ToLower()});");
     }
   }
 }
--- src/GTMH.S11n/IS11nVisitor.cs
+++ src/GTMH.S11n/IS11nVisitor.cs
@@ -7,7 +7,7 @@ namespace GTMH.S11n
   public interface IS11nVisitor
   {
     void VisitMember(string a_Name, bool a_Required);
-    void VisitInstance(string a_Name, bool a_Required);
-    void VisitInstanceList(string a_Name, bool a_Required);
+    void VisitInstance(string a_Name, string a_Type, bool a_Required);
+    void VisitInstanceList(string a_Name, string a_Type, bool a_Required);
   }
 }
